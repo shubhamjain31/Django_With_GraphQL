db1997b06d5a70bae57826e661156ea61fea9aef
shubhamjain31 <sj27754@gmail.com>
2022-07-28 22:55:23 +0530

    displayed all data

--- frontend/src/pages/Authors.js
+++ frontend/src/pages/Authors.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import useDocumentTitle from './useDocumentTitle';
 
 import './Inner.css';
@@ -11,37 +11,40 @@ export default function Authors(props) {
     useDocumentTitle('Authors');
 
     const { loading, error, data } = useQuery(GET_AUTHORS);
-    console.log(data.allAuthors)
+
+    if (loading) {
+      return <div>Loading...</div>
+    }
+    if (error) {
+        return <div>Error! {error.message}</div>
+    }
    
-  return data.allAuthors.map(({ id, name, biodata }) => (
+  return (
     <div className="inner-form-container">
       <div className="inner-form">
-        <div className="inner-form-content" key={id}>
+        <div className="inner-form-content">
           <h3 className="inner-form-title">Authors</h3>
           <Table striped bordered hover>
             <thead>
                 <tr>
-                <th>#</th>
+                <th>SNo.</th>
                 <th>Name</th>
                 <th>Biodata</th>
+                <th>Action</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                <td>1</td>
-                <td>Mark</td>
-                <td>Otto</td>
-                </tr>
-                <tr>
-                <td>2</td>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                </tr>
+            {
+                data.allAuthors?.map((alldata, index) => {
+                  return <tr key={alldata.id}><td>{index+1}</td><td>{alldata?.name}</td><td>{alldata?.biodata }</td><td>
+                    <button className="btn btn-success btn-sm px-3">Edit</button><button className="btn btn-danger my-2 btn-sm">Delete</button></td></tr>
+            })
+            }
             </tbody>
             </Table>
           
         </div>
       </div>
     </div>
-  ));
+  );
 }
